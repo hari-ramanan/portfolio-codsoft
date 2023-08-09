5ab071f3d72015529d8045e5e8c8670f3121eb53
Hari Ramanan <122186563+hariramanansaravanan@users.noreply.github.com>
2023-08-09 19:17:31 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+
+const mobilemenubutton = document.getElementById('mobile-menu-button');
+const mobilemenu = document.getElementById('mobile-menu');
+        
+    mobilemenubutton.addEventListener('click', () => {
+         mobilemenu.classList.toggle('hidden');
+});
+
